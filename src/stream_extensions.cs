f50eac53c8fc3f2be83c62ff743685b1499ea6bd
Someshwar Roychowdhury <someshwar@Someshwars-MacBook-Air.local>
2025-03-05 22:34:04 +0300

    CLI tests (cli-test-1741203244184)

--- src/stream_extensions.cs
+++ src/stream_extensions.cs
@@ -23,17 +23,70 @@ public static class StreamExtensions
         public short max_version;
     }
 
-    public static async Task HandleApiVersion(this NetworkStream stream, int api_key) {
+
+    public static async Task HandleApiVersionRequest(this NetworkStream stream, int correlation_id, int api_key, int api_version) {
         // response to handle ApiVersion request
-        if(api_key==18) {
-            await stream.WriteInt8BigEndianAsync(2); // api_key array length 1 byte
-            await stream.WriteInt16BigEndianAsync(18); // api_key 2 bytes
-            await stream.WriteInt16BigEndianAsync(4); // min_version 2 bytes
-            await stream.WriteInt16BigEndianAsync(4); // max_version 2 bytes
-            await stream.WriteInt8BigEndianAsync(0); // tagBuffer 1 byte
-            await stream.WriteInt32BigEndianAsync(0); // throttleTimeMs 4 bytes
-            await stream.WriteInt8BigEndianAsync(0); // tagBuffer 1 byte
+        List<ApiKey> keys = new List<ApiKey>
+        {   new ApiKey {api_key = 18, min_version = 4, max_version = 4},
+            new ApiKey {api_key = 75, min_version = 0, max_version = 0}
+        };
+
+        int keys_length_bytes = 6; // 2 bytes for api_key + 2 bytes for min_version + 2 bytes for max_version
+        byte api_key_array_length = (byte)(keys.Count + 1);
+        // 4 bytes response_size (not considered in defining response length)
+        // 4 bytes for correlation_id + 2 bytes for error_code + 1 byte for api_key array length 
+        // + (api_key_array_length * keys_length_bytes) + 1 byte for tagBuffer 
+        // + 4 bytes for throttleTimeMs + 1 byte for tagBuffer
+        int response_size = 4 + 2 + 1 + (api_key_array_length * keys_length_bytes) + 1 + 4 + 1;
+
+        await stream.WriteInt32BigEndianAsync(response_size); // 4 bytes - response size excluding the size field itself
+        await stream.WriteInt32BigEndianAsync(correlation_id); // correlation_id 4 bytes
+        await stream.WriteInt16BigEndianAsync(CheckApiVersion(api_version)); // error_code 2 bytes
+        await stream.WriteInt8BigEndianAsync(api_key_array_length); // api_key array length 1 byte
+        foreach(ApiKey key in keys) {
+            await stream.WriteInt16BigEndianAsync(key.api_key); // api_key 2 bytes
+            await stream.WriteInt16BigEndianAsync(key.min_version); // min_version 2 bytes
+            await stream.WriteInt16BigEndianAsync(key.max_version); // max_version 2 bytes
         }
+        await stream.WriteInt8BigEndianAsync(0); // tagBuffer 1 byte
+        await stream.WriteInt32BigEndianAsync(0); // throttleTimeMs 4 bytes
+        await stream.WriteInt8BigEndianAsync(0); // tagBuffer 1 byte
+        // if(api_key==18) {
+        //     await stream.WriteInt8BigEndianAsync(2); // api_key array length + 1 -  1 byte
+        //     foreach(ApiKey key in keys) {
+        //         await stream.WriteInt16BigEndianAsync(key.api_key); // api_key 2 bytes
+        //         await stream.WriteInt16BigEndianAsync(key.min_version); // min_version 2 bytes
+        //         await stream.WriteInt16BigEndianAsync(key.max_version); // max_version 2 bytes
+        //     }
+        //     await stream.WriteInt8BigEndianAsync(0); // tagBuffer 1 byte
+        //     await stream.WriteInt32BigEndianAsync(0); // throttleTimeMs 4 bytes
+        //     await stream.WriteInt8BigEndianAsync(0); // tagBuffer 1 byte
+        // }
+        // // response to handle DescribeTopicPartitions API request
+        // if(api_key==75) {
+        //     await stream.WriteInt8BigEndianAsync(2); // api_key array length 1 byte
+        //     await stream.WriteInt16BigEndianAsync(75); // api_key 2 bytes
+        //     await stream.WriteInt16BigEndianAsync(0); // min_version 2 bytes
+        //     await stream.WriteInt16BigEndianAsync(0); // max_version 2 bytes
+        //     await stream.WriteInt8BigEndianAsync(0); // tagBuffer 1 byte
+        //     await stream.WriteInt32BigEndianAsync(0); // throttleTimeMs 4 bytes
+        //     await stream.WriteInt8BigEndianAsync(0); // tagBuffer 1 byte
+        // }
+
+    }
+
+    public static async Task HandleDescribeTopicPartition(this NetworkStream stream, int api_key) {
+            // topics array length - 1 byte
+                // length(name) + 1 - varint 2 bytes
+                // name - length(name) bytes
+            // tag_buffer - 1 byte
+            // response_partition_limit - INT32 - 4 bytes
+            // topic_name array length - 1 byte
+                // length(topic_name) + 1 - varint 2 bytes
+                // topic_name - length(name) bytes
+                // partition_index - INT32 - 4 bytes
+            // tag_buffer - 1 byte
+            await Task.Delay(0);
     }
 
     public static async Task WriteInt8BigEndianAsync(this NetworkStream stream, byte input) 
@@ -62,7 +115,7 @@ public static class StreamExtensions
         
         return error_code;
     }
-    public static async Task ParseHeader(this NetworkStream stream) {
+    public static async Task ParseRequest(this NetworkStream stream) {
         byte[] binaryData = new byte[1024];
         
         int bytesRead = await stream.ReadAsync(binaryData, 0, binaryData.Length);    
@@ -80,16 +133,16 @@ public static class StreamExtensions
         Console.WriteLine($"API Version: {api_version}");
         // Console.WriteLine($"API Version Error Code: {CheckApiVersion(api_version)}");
         // 23 bytes - 4 bytes message_length + 4 bytes correlation_id + 2 bytes error_code + 13 bytes APIVersion Response
-        await stream.WriteInt32BigEndianAsync(19); // 4 bytes - response size excluding the size field itself
-        await stream.WriteInt32BigEndianAsync(correlation_id); // 4 bytes
-        await stream.WriteInt16BigEndianAsync(CheckApiVersion(api_version)); // error_code 2 bytes
+        // await stream.WriteInt32BigEndianAsync(19); // 4 bytes - response size excluding the size field itself
+        // await stream.WriteInt32BigEndianAsync(correlation_id); // 4 bytes
+        // await stream.WriteInt16BigEndianAsync(CheckApiVersion(api_version)); // error_code 2 bytes
 
-        await stream.HandleApiVersion(api_key);
+        await stream.HandleApiVersionRequest(correlation_id, api_key, api_version);
     }
 
     async public static Task ParseApiVersionRequest(this NetworkStream stream)
     {
-        await stream.ParseHeader();
+        await stream.ParseRequest();
         await stream.FlushAsync();
     }
 }
